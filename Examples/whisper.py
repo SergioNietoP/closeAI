82d58babb00f73a0b47b4ab05402c6a7d1f328db
SergioNietoP <sergionietop@gmail.com>
2023-06-28 13:03:59 +0200

    Whisper Approach

--- Examples/whisper.py
+++ Examples/whisper.py
@@ -1,10 +1,10 @@
 import openai
 
 # Set the API key
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Open the audio file and transcribe it
-with open("NDkyODU2MDEwNDkyODcy_Hp5xdlTKoq8.mp3", "rb") as audio_file:
+with open("Examples/answer_2.mp3", "rb") as audio_file:
     transcript = openai.Audio.transcribe(
         file = audio_file,
         model = "whisper-1",
@@ -13,3 +13,17 @@ with open("NDkyODU2MDEwNDkyODcy_Hp5xdlTKoq8.mp3", "rb") as audio_file:
 
 # Print the transcript
 print(transcript)
+
+question_1 = " Imagine you're leading a team that has a conflict about the direction of a project. How would you resolve this situation?"
+
+completion = openai.ChatCompletion.create(
+  model = "gpt-3.5-turbo",
+  temperature = 0.8,
+  max_tokens = 2000,
+  messages = [
+    {"role": "system", "content": "You are the best creativity test evaluator. You are going to receive an answer from a candidate that is applying for a job to this question : '" + question_1 + "'. I need you to give scores based on this metrics: Specificity, Relevance, Problem-Solving, Initiative, Outcome, Self-Reflection, Creativity, Communication. Also give me a short explanation of your evaluation with pros and cons. Give an answer in a json format where each field is a metric, and also add average score and the explanation fields." },
+    {"role": "user", "content": transcript}
+  ]
+)
+
+print(completion.choices[0].message.content)
